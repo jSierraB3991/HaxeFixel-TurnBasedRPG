e1d36bfad573891a083a1986feab2e259c58bdf1
Elon Mush <eliotandelon@gmail.com>
2021-06-09 16:49:33 -0500

    Groundwork

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,11 +5,14 @@ import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	var player:Player;
+
 	override public function create()
 	{
 		var text = new FlxText(10, 10, 100, "Hello World");
 		add(text);
-
+		var player = new Player(20, 20);
+		add(player);
 		super.create();
 	}
 

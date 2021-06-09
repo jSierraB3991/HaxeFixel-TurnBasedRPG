ae4a433466dc95a524c463b4c50a28a0ee85af7d
Elon Mush <eliotandelon@gmail.com>
2021-06-09 16:02:10 -0500

    test project

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,15 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var text = new FlxText(10, 10, 100, "Hello World");
+		add(text);
+
 		super.create();
 	}
 
